e1dfae9708489a82b8f2e180b2fb890babe27c76
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-01-21 17:56:59 +0530

    Created Stock struct in model

---
+++ models/model.go
@@ -0,0 +1,12 @@
+package model
+
+type Stock struct{
+	StockID int64  `json:"stockid"`
+	Name string `json:"name"`
+	Price int64 `json:"price"`
+	Company string  `json:"company"`
+}
+
+func model(){
+
+}
